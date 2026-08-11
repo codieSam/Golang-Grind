f0a45284a1ed3b9d44cbf6226c6912dc7519ab05
codieSam <samratbelbase408@gmail.com>
2026-08-11 14:11:00 +0100

    Initial commit

---
+++ 1_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+	//int
+	fmt.Println(1 + 1)
+	//string
+	fmt.Println("Hello Golang !")
+	//bool
+	fmt.Println(true)
+	//floats
+	fmt.Println(10.6 / 3.6)
+}
