07ec28e6a57c2076ed2c9a247da92bfa86e9bc1e
codieSam <samratbelbase408@gmail.com>
2026-08-25 01:26:25 +0100

    Arrays in Go

---
+++ 7_arrays/arrays.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// names := [5]string{"Alice", "Bob", "Charlie", "David", "Eve"}
+	// fmt.Println(names[0])
+	// fmt.Println(len(names))
+
+	//2D array
+	matrix := [2][3]int{{1, 2}, {3, 4, 5}}
+	fmt.Println(matrix)
+}
