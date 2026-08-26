35e795a893e96e4eb317da5ca1db5ff981df0e0e
codieSam <samratbelbase408@gmail.com>
2026-08-26 13:57:27 +0100

    ok keyword in maps with if else

--- 9_maps/maps.go
+++ 9_maps/maps.go
@@ -1,19 +1,42 @@
 package main
 
-import "fmt"
-
 //maps -> hash, object, dict
+import (
+	"fmt"
+)
 
 func main() {
-	//creating map
+	// //creating map
 
-	m := make(map[string]string)
+	// m := make(map[string]string)
 
-	// setting an elements
-	m["name"] = "Golang"
-	m["area"] = "Backend"
-	//get an element
+	// // setting an elements
+	// m["name"] = "Golang"
+	// m["area"] = "Backend"
+	// //get an element
 
-	fmt.Println(m["name"], m["area"], m["version"])
+	// fmt.Println(m["name"], m["area"], m["version"])
+
+	// m := make(map[string]int)
+
+	// m["Volvo"] = 2010
+	// m["BMW"] = 2015
+	// m["Ford"] = 2005
+	// delete(m, "Ford")
+	// // clear(m)
+	// fmt.Println(len(m))
+	// fmt.Println(n)
+
+	// fmt.Println(m["Volvo"], m["BMW"], m["Ford"], m["name"])
+
+	n := map[string]int{"Volvo": 2010, "BMW": 2015, "Ford": 2005}
+
+	v, ok := n["Ford"]
+	fmt.Println(v)
+	if ok {
+		fmt.Println("It's okay")
+	} else {
+		fmt.Println("It's not okay")
+	}
 
 }
