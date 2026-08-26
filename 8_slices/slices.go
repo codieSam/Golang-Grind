2510704daeeeea144105a4bd828af48948f5e4f7
codieSam <samratbelbase408@gmail.com>
2026-08-26 01:31:25 +0100

    slice operator & slice package in go

--- 8_slices/slices.go
+++ 8_slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	// var nums = []int{1, 2, 3, 4, 5}
@@ -16,13 +19,25 @@ func main() {
 
 	//copy function
 
-	var nums2 = make([]int, 0, 5)
-	nums2 = append(nums2, 2)
-	var nums3 = make([]int, len(nums2))
+	// var nums2 = make([]int, 0, 5)
+	// nums2 = append(nums2, 2)
+	// var nums3 = make([]int, len(nums2))
 
-	//copy
-	copy(nums3, nums2)
+	// //copy
+	// copy(nums3, nums2)
 
-	fmt.Println(nums2, nums3)
+	// fmt.Println(nums2, nums3)
+
+	// //slice operator
+
+	// nums := []int{0, 1, 2, 3, 4, 5}
+	// fmt.Println(nums[:3])
+
+	//slice package
+
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+
+	fmt.Println(slices.Equal(nums1, nums2))
 
 }
