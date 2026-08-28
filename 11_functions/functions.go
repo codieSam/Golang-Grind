b18bff5ededc9a4e29b4579d6265e62db9208614
codieSam <samratbelbase408@gmail.com>
2026-08-28 12:10:02 +0100

    Return keyword in go functions

--- 11_functions/functions.go
+++ 11_functions/functions.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func myMessage(fName string, Age int) {
-	fmt.Println("Hello", fName, "you are", Age, "years old")
+// func myMessage(fName string, Age int) {
+// 	fmt.Println("Hello", fName, "you are", Age, "years old")
+// }
+
+// func main() {
+// 	myMessage("John", 25)
+// }
+
+// Return keyword is used to return a value from a function
+
+func myMessage(fname string, Age int) string {
+	return ("Hello " + fname + ", you are " + fmt.Sprintf("%d", Age) + " years old.")
 }
 
 func main() {
-	myMessage("John", 25)
+	fmt.Println(myMessage("John", 25))
 }
