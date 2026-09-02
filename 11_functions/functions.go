4eb6ffa9affbc56140faa0e40179eeed4b08cdfa
codieSam <samratbelbase408@gmail.com>
2026-09-02 02:07:22 +0100

    Variadic functions in golang

--- 11_functions/functions.go
+++ 11_functions/functions.go
@@ -12,10 +12,23 @@ import "fmt"
 
 // Return keyword is used to return a value from a function
 
-func myMessage(fname string, Age int) string {
-	return ("Hello " + fname + ", you are " + fmt.Sprintf("%d", Age) + " years old.")
+// func myMessage(fname string, Age int) string {
+// 	return ("Hello " + fname + ", you are " + fmt.Sprintf("%d", Age) + " years old.")
+// }
+
+// func main() {
+// 	fmt.Println(myMessage("John", 25))
+// }
+
+//Recursive function is a function that calls itself & reach to a base case to stop the recursion
+
+func myNumbers(num int) int {
+	if num == 11 {
+		return 0
+	}
+	return (num + 1)
 }
 
 func main() {
-	fmt.Println(myMessage("John", 25))
+	fmt.Println(myNumbers(0))
 }
