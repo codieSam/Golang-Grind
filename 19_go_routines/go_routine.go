cfd68e395cd825d534f8449e9d6b5cd6756e228e
codieSam <samratbelbase408@gmail.com>
2026-09-04 13:15:08 +0100

    WaitGrouo in Go with examples

--- 19_go_routines/go_routine.go
+++ 19_go_routines/go_routine.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done() // this will tell the waitgroup that this go routine is done and it can decrement the counter by 1
 	fmt.Println("Doing task", id)
 }
 
 func main() {
+
+	var wg sync.WaitGroup
+
 	for i := 0; i < 10; i++ {
-		go task(i) //it will run all 11 functions paralelly and it will not wait for the function to complete
+		wg.Add(1)
+		go task(i, &wg) //it will run all 11 functions paralelly and it will not wait for the function to complete
+
 	}
 
-	time.Sleep(time.Second * 2)
+	wg.Wait() // this will wait for all the go routines to complete before exiting the main function
+
+	// time.Sleep(time.Second * 2) // instead of this we use waitgroup to wait for all the go routines to complete before exiting the main function
 
 }
