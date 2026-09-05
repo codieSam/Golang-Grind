2babac2f2e6172cce35aa9e51389edb491939339
codieSam <samratbelbase408@gmail.com>
2026-09-05 01:51:45 +0100

    Implementing email queing system using channels in GO

--- 20_channels/channels.go
+++ 20_channels/channels.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //sending
 
@@ -15,20 +18,59 @@ import "fmt"
 // }
 
 // receiving
-func sum(result chan int, num1 int, num2 int) {
-	sumResult := num1 + num2
-	result <- sumResult
+// func sum(result chan int, num1 int, num2 int) {
+// 	sumResult := num1 + num2
+// 	result <- sumResult
+// }
+
+// Go routine synchronizer
+
+// func task(done chan bool) {
+// 	defer func() { done <- true }()
+// 	fmt.Println("processing task...")
+// 	// done <- true
+// }
+
+func emailSender(emailChan chan string, done chan bool) {
+	defer func() { done <- true }()
+	for email := range emailChan {
+		fmt.Println("Sending email to ", email)
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
 
-	result := make(chan int)
+	emailChan := make(chan string, 100)
+	done := make(chan bool)
 
-	go sum(result, 4, 5)
+	go emailSender(emailChan, done)
 
-	res := <-result
+	for i := 0; i < 100; i++ {
+		emailChan <- fmt.Sprintf("%d@gmail.com", i)
+	}
 
-	fmt.Println("Sum is", res)
+	fmt.Println("Done sending")
+	<-done
+	// emailChan <- "1@example.com"
+	// emailChan <- "2@gmail.com"
+
+	// fmt.Println(<-emailChan)
+	// fmt.Println(<-emailChan)
+
+	// done := make(chan bool)
+
+	// go task(done)
+
+	// <-done //block the main function until the task is done and it will wait for the task to complete before exiting the main function
+
+	// result := make(chan int)
+
+	// go sum(result, 4, 5)
+
+	// res := <-result
+
+	// fmt.Println("Sum is", res)
 
 	// numChan := make(chan int)
 
