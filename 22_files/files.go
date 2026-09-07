37e15f7851febbaee1f20c26f54fe4f6c74c77f8
codieSam <samratbelbase408@gmail.com>
2026-09-07 01:40:32 +0100

    Reading the file using filesystem in Golang

--- 22_files/files.go
+++ 22_files/files.go
@@ -6,19 +6,37 @@ import (
 )
 
 func main() {
+	// f, err := os.Open("example.txt")
+	// if err != nil {
+	// 	// log the error
+	// 	panic(err)
+	// }
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	//log the error
+	// 	panic(err)
+	// }
+
+	// fmt.Println("File Name: ", fileInfo.Name())
+	// fmt.Println("File Size: ", fileInfo.Size())
+	// fmt.Println("File Last modifies on: ", fileInfo.ModTime())
+
+	// read file
+
 	f, err := os.Open("example.txt")
+
 	if err != nil {
-		// log the error
-		panic(err)
-	}
-	fileInfo, err := f.Stat()
-	if err != nil {
-		//log the error
 		panic(err)
 	}
 
-	fmt.Println("File Name: ", fileInfo.Name())
-	fmt.Println("File Size: ", fileInfo.Size())
-	fmt.Println("File Last modifies on: ", fileInfo.ModTime())
+	defer f.Close()
+	buff := make([]byte, 10)
+	d, err := f.Read(buff)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(buff); i++ {
+		fmt.Println("Data: ", d, string(buff[i]))
+	}
 
 }
