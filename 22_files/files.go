45c5ccf58e31f7cc6909a7831f81223b906f2c4e
codieSam <samratbelbase408@gmail.com>
2026-09-07 15:00:44 +0100

    Using reader & writer functions to rxmplore more and remove function for deletion of files

--- 22_files/files.go
+++ 22_files/files.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -41,28 +42,89 @@ func main() {
 
 	// we can simply read the file using ReadFile method
 
-	data, err := os.ReadFile("example.txt")
+	// data, err := os.ReadFile("example.txt")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(string(data))
+
+	// // Read folders
+
+	// dir, err := os.Open("../")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer dir.Close()
+
+	// fileInfo, err := dir.ReadDir(-1)
+
+	// for _, fi := range fileInfo {
+	// 	fmt.Println(fi.Name(), fi.IsDir())
+	// }
+
+	// Create a file
+
+	f, err := os.Create("example2.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString("Hi Go. ")
+	f.WriteString("Nice language")
+
+	bytes := []byte("Hello Golang")
+
+	f.Write(bytes)
+
+	// Read and write to another file (Streaming fashion)
+
+	sourceFIle, err := os.Open("example.txt")
 
 	if err != nil {
 		panic(err)
 	}
+	defer sourceFIle.Close()
 
-	fmt.Println(string(data))
-
-	// Read folders
-
-	dir, err := os.Open("../")
-
+	destFile, err := os.Create("example2.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	defer dir.Close()
+	defer destFile.Close()
 
-	fileInfo, err := dir.ReadDir(-1)
+	reader := bufio.NewReader(sourceFIle)
+	writer := bufio.NewWriter(destFile)
 
-	for _, fi := range fileInfo {
-		fmt.Println(fi.Name(), fi.IsDir())
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			if err.Error() != "EOF" {
+				panic(err)
+			}
+			break
+		}
+
+		e := writer.WriteByte(b)
+		if err != nil {
+			panic(e)
+		}
+
 	}
+	writer.Flush()
+	fmt.Println("Written to new file successfully")
+
+	// Delete a file
+
+	er := os.Remove("example2.txt")
+	if er != nil {
+		panic(er)
+	}
+
+	fmt.Println("File deleted successfully")
 
 }
